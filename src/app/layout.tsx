43260c3d2086f4e366452c2ee03f89020b1c74ac
Amirreza <Amirrezakrym@gmail.com>
2024-04-11 16:43:38 +0430

    end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Todo App 2",
+  title: "Todo App",
   description: "Amirrezakrym@gmail.com",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,68 @@
-import Image from "next/image";
+'use client'
+export interface TodoObject {
+  id: number,
+  value: string,
+  done:boolean
+}
 
-export default function Home() {
+import Image from "next/image";
+import { useRef, useEffect, useState } from "react";
+
+
+const Home = () => {
+
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<TodoObject[]>([]);
+  
+  const addTodo = () =>{
+    setTodos([{ id: 1, value: todo, done: false }, ...todos])
+    setTodo("")
+  }
+
+  const markTodoDone = () =>{
+   
+   }
+   
+  const inputRef = useRef<HTMLInputElement>(null!)
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+  
+
   return (
     <>
     <header className="bg-slate-950 p-4">
-      <h1 className="text-3xl">Todos 2</h1>
+      <h1 className="text-3xl">Todos</h1>
     </header>
     <main className="p-4">
       <input 
+      ref={inputRef}
       type="text" 
       placeholder="Enter a new todo"
       className="p-2 rounded mr-5 text-slate-900"
-      // onChange={(e) => setTodo(e.target.value)}
-      // value={todo}
+      onChange={(e) => setTodo(e.target.value)}
+      value={todo}
       />
       <button 
       type="button"
       className="border-2 p-2 rounded"
-      // onClick={() => addTodo()}
+      onClick={() => addTodo()}
       >Add Todo</button>
       <ul className="mt-5">
-        {/* {
-          todos.map(todo => (
+        {
+           todos.map(todo => (
             <li
-            onClick={() => markTodoDone(todo.id)}
-             className={`text-3xl ml-5 cursor-pointer ${todo.done ? 'line-through':'no-underline'}`}>
+            onClick={() => markTodoDone()}
+             className={`text-3xl ml-5 cursor-pointer`}>
               {todo.value}
-            </li>
+             </li>
           ))
-        } */}
+        }
         
       </ul>
     </main>
     </>
   );
 }
+
+export default Home
